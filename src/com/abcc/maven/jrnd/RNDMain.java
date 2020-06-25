fa6f9bf18e104166f53407aa789711d8ba186f83
kvjyothish <kvjyothish@jyothishs-air.fios-router.home>
2020-06-25 13:38:33 -0400

    Updating from Mac.

--- src/com/abcc/maven/jrnd/RNDMain.java
+++ src/com/abcc/maven/jrnd/RNDMain.java
@@ -1,25 +1,41 @@
 package com.abcc.maven.jrnd;
 
-import java.lang.annotation.Repeatable;
-
 //import com.abcc.maven.jrnd.JavaRandD;
 
 public class RNDMain {
 
 	public static void main(String[] args) {
 		
-		//Testing Relational Operators
-		int Zero = 0, One = 1, Twoo = 2, Three = 3;
-		boolean bResult1 = One > Zero && Twoo < Three;
-		System.out.println("\nOne is greater than Two: " + bResult1);
+		//TestEnhLoop();
+		Fruits Apple;
+		Apple = new Fruits();
 		
+		Apple.sColor = "Green";
+		Apple.bSweet = true;
+		Apple.iWeight = 100;
+		
+		Apple.showCharacteristics();
+		
+	}
+		
+	public static void testRO() {
+		
+		//Testing Relational Operators
+		int Zero = 0, One = 1, Two = 2, Three = 3;
+		boolean bResult1 = One > Zero && Two < Three;
+		System.out.println("\nOne is greater than Two: " + bResult1);
+	}
+	
+	public static void testLO() {
 		//Testing Logical Operator
 		boolean bTrue = true;
 		boolean bFalse = false;
 		System.out.println("\nCheck if both the boolean variables are true : " + (bTrue && bFalse));
 		System.out.println("Check if even one of the boolean varibale is true : " + (bTrue || bFalse));
 		System.out.println("Change the state of the bTrue to false : " + (!bTrue));
-
+	}
+	
+	public static void testCO() {
 		//Testing Conditional Operator
 		boolean bResult2; 
 		int Ten = 10, Two = 2;
@@ -27,6 +43,9 @@ public class RNDMain {
 		
 		System.out.println("\nConditional Operator Test result is: " + bResult2);
 		
+	}
+	
+	public static void testIFELSE() {
 		//Testing if then else statement
 		int val1 = 10000, val2 = 5000;
 		String sText = "Continue";	
@@ -42,7 +61,9 @@ public class RNDMain {
 			System.out.println("\nThis is FALSE!");
 			
 			}
-		
+	}
+	
+	public static void testSwitchCase() {
 		//Testing switch...case...default
 		int iNumber = 10000000;
 		String sWord;
@@ -73,7 +94,9 @@ public class RNDMain {
 		}
 		
 		System.out.println("\nSelected value is " +"'" + sWord + "'.");
-		
+	}
+	
+	public static void testArray() {
 		//Testing arrays
 		//Array style 1
 		String[] aMonth = new String[13];
@@ -95,7 +118,7 @@ public class RNDMain {
 		for (int i=1; i< 13; i++) {
 			System.out.println(i +". "+ aMonth[i]);
 		}
-		
+	
 		//Array style 2
 		String[] aDay = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
@@ -112,7 +135,9 @@ public class RNDMain {
 			sList = sList + iDay[i] + ", "  ;
 		}
 		System.out.println(sList);
-		
+	}
+	
+	public static void testWhileLoop() {
 		//Testing while loop
 		int iCount=0;
 		
@@ -127,7 +152,9 @@ public class RNDMain {
 				System.out.println("Skipping Count-"+iCount);
 			}
 		}
-		
+	}
+	
+	public static void testDOWhile() {
 		//Testing do{...}while (expression) loop
 		int Count = 0;
 		 // This will print -- 5,10,15,20,25
@@ -136,14 +163,34 @@ public class RNDMain {
 		 System.out.println("Count is ==> "+ Count);
 		 }while(Count < 25);
 		 
-		 int CountOnce = 25;
+		 int CountOnce = 0;
 		 System.out.println("<==== Next Count ====>");
 		 // This will just print once 
 		 do{
 		 CountOnce = CountOnce + 5; 
-		 System.out.println("Count is ==> "+ CountOnce);
-		 }while(CountOnce < 25);
 		 
-		//Add here
-		}
+		if (CountOnce == 15) {
+			System.out.println("Skipping CountOnce " + CountOnce);
+			continue;
+			}
+		 System.out.println("CountOnce is ==> "+ CountOnce);
+		 }while(CountOnce < 100);
 	}
+		
+	public static void TestEnhLoop() {
+		// Array of String storing days of the week
+	     String days[] = { "Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"};
+	 
+	     // Enhanced for loop, this will automatically iterate on the array list 
+	     for (String dayName : days) {
+	       System.out.println("Days ==> "+ dayName);
+	     	}
+	 
+	     System.out.println("<==== Normal For Loop ====>");
+	     // Normal for loop
+	     for (int i=0; i < days.length; i++) {
+	         System.out.println("Days ==> "+ days[i]);
+	}
+		//Add new methods up/down
+		}
+}
